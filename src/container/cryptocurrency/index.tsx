1f7a985da3ca23fb791157f0fa3490c73c3267ae
Dedicker Remi <dedickerremi@gmail.com>
2021-04-26 23:24:58 +0100

    add container

---
+++ src/container/cryptocurrency/index.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react';
+import { ACTIONS, useCryptoDispatch, useCryptoState } from '../../context/CryptoCurrenciesContext';
+import styled from 'styled-components';
+import CryptoTable from '../../components/CryptoTable';
+import { Button, Container } from '../../style/generic';
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Home: FC = () => {
+    const {cryptoCurrencies} = useCryptoState()
+    const dispatch = useCryptoDispatch()
+    
+    const handleClick = () => {
+        dispatch({ type: ACTIONS.RESET_FAVORITE });
+    }
+    const cryptoCurrenciesAsFavorite = cryptoCurrencies && cryptoCurrencies.filter(e => e.isFavorite)
+
+    return (
+        <Main>
+            <Container>
+                <CryptoTable cryptoCurrencies={cryptoCurrencies} />
+            </Container>
+            {
+                cryptoCurrenciesAsFavorite && cryptoCurrenciesAsFavorite.length > 0 &&
+                <Container>
+                        <Button onClick={handleClick} css={`float: right; margin: 10px 0`} disabled={cryptoCurrenciesAsFavorite?.length === 0}>Delete them All</Button>
+                        <CryptoTable cryptoCurrencies={cryptoCurrenciesAsFavorite} isFavoriteTable={true} />
+                </Container>
+            }
+        </Main>
+    )
+}
+
+export default Home
